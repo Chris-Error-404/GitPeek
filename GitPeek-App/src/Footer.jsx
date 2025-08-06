934dd30769e1fc5b8b2f3fccd15f3b0ee95696a5
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-08-06 12:21:31 +0100

    footer edit

--- GitPeek-App/src/Footer.jsx
+++ GitPeek-App/src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <>
-            <footer className="mt-2 px-4 py-4 flex flex-col-reverse lg:flex-row items-center justify-between  border-t border-t-[var(--tag)] lg:gap-0 gap-2">
+            <footer className="mt-2 px-4 py-4 flex flex-col-reverse lg:flex-row items-center justify-between  border-t border-t-[var(--tag)] lg:gap-0 gap-4">
                 <p className="font-meduim text-[var(--foreground)] text-center text-xs">&copy; <a href="https://github.com/Chris-Error-404/GitPeek?tab=GPL-3.0-1-ov-file">Typical Developer</a>  | All Rights Reserved</p>
                 <ul className="text-[var(--foreground)] flex items-center gap-2.5">
                     <li><a href="https://github.com/Chris-Error-404" target="_blank" rel="noopener" className="cursor-pointer"><i className="bx bxl-github text-3xl lg:text-xl"></i></a></li>
