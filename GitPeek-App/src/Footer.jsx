5b7bc49781bf0dd89bc7cc90c17b77c40acf86d2
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-08-06 12:03:27 +0100

    footer links

--- GitPeek-App/src/Footer.jsx
+++ GitPeek-App/src/Footer.jsx
@@ -2,12 +2,12 @@ function Footer(){
     return(
         <>
             <footer className="mt-2 px-4 py-4 flex items-center justify-between  border-t border-t-[var(--tag)]">
-                <p className="font-meduim text-[var(--foreground)] text-center text-xs">&copy; <a href="#">Typical Developer</a>  | All Rights Reserved</p>
+                <p className="font-meduim text-[var(--foreground)] text-center text-xs">&copy; <a href="https://github.com/Chris-Error-404/GitPeek?tab=GPL-3.0-1-ov-file">Typical Developer</a>  | All Rights Reserved</p>
                 <ul className="text-[var(--foreground)] flex items-center gap-2.5">
-                    <li><a href="" className="cursor-pointer"><i className="bx bxl-github text-xl"></i></a></li>
-                    <li><a href="" className="cursor-pointer"><i className="bx bxl-twitter text-xl"></i></a></li>
-                    <li><a href="" className="cursor-pointer"><i className="bx bxl-discord text-xl"></i></a></li>
-                    <li><a href="" className="cursor-pointer"><i className="bx bx-envelope text-xl"></i></a></li>
+                    <li><a href="https://github.com/Chris-Error-404" target="_blank" rel="noopener" className="cursor-pointer"><i className="bx bxl-github text-xl"></i></a></li>
+                    <li><a href="https://x.com/Chris_Error_404" target="_blank" rel="noopener" className="cursor-pointer"><i className="bx bxl-twitter text-xl"></i></a></li>
+                    <li><a href="https://discord.gg/AJUzJ5dq" target="_blank" rel="noopener" className="cursor-pointer"><i className="bx bxl-discord text-xl"></i></a></li>
+                    <li><a href="mailto:nzenwatachristopher186@gmail.com" rel="noopener" className="cursor-pointer"><i className="bx bx-envelope text-xl"></i></a></li>
                 </ul>
             </footer>
         </>
