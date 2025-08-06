2da3b4a45550f028cf25f50aa57eea0688e6e6cf
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-08-06 12:53:56 +0100

    peek button w-fit for all screen sizes

--- GitPeek-App/src/components/SolidButton.jsx
+++ GitPeek-App/src/components/SolidButton.jsx
@@ -1,7 +1,7 @@
 function SolidButton(props){
     return(
         <>
-        <button className="px-3 py-2 bg-[var(--foreground)] border border-transparent text-sm font-medium flex items-center justify-center gap-1.5 lg:w-fit w-[100%] rounded-sm transition-all duration-200 hover:bg-transparent hover:border-[var(--border)] hover:text-[var(--foreground)] cursor-pointer"><i className='bx bx-show'></i>{props.buttonText}</button>
+        <button className="px-3 py-2 bg-[var(--foreground)] border border-transparent text-sm font-medium flex items-center justify-center gap-1.5 lg:w-fit rounded-sm transition-all duration-200 hover:bg-transparent hover:border-[var(--border)] hover:text-[var(--foreground)] cursor-pointer"><i className='bx bx-show'></i>{props.buttonText}</button>
         </>
     );
 }
