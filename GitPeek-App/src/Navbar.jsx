d908c5dfa0ff8d6566c916dfe93c40d5f2d03ab7
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-08-08 14:44:47 +0100

    navbar transition & duration

--- GitPeek-App/src/Navbar.jsx
+++ GitPeek-App/src/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar({ onToggleDark, isDark }){
     return(
         <>
-        <nav className="backdrop-blur-lg bg-[var(--forekground)]/30 dark:bg-[var(--background)]/30 sticky top-0 z-10">
+        <nav className="backdrop-blur-lg bg-[var(--forekground)]/30 dark:bg-[var(--background)]/30 sticky top-0 z-10 transition-all duration-200">
             <div className="mx-auto max-w-[1400px] px-4 py-3.5 flex items-center justify-between">
             <a href="../index.html" className="text-[var(--background)] dark:text-[var(--foreground)] text-xl font-semibold"><i className="bx bxl-github"></i> GitPeek</a>
 
