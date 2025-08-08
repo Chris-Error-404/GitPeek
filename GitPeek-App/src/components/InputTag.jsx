ff66b1ee6487b5f47aec4b6c71e756e7b5ff7e8b
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-08-08 12:48:33 +0100

    input transition duration

--- GitPeek-App/src/components/InputTag.jsx
+++ GitPeek-App/src/components/InputTag.jsx
@@ -1,7 +1,7 @@
 function InputTag(props){
     return(
         <>
-        <input type="text" className="border border-[var(--lighter-tag)] dark:border-[var(--tag)] px-2 py-2 rounded-sm text-sm placeholder:text-[var(--background)] dark:placeholder:text-[var(--foreground)]" placeholder={props.placeholderText} required />
+        <input type="text" className="border border-[var(--lighter-tag)] dark:border-[var(--tag)] px-2 py-2 rounded-sm text-sm placeholder:text-[var(--background)] dark:placeholder:text-[var(--foreground)] transition-all duration-200" placeholder={props.placeholderText} required />
         </>
     );
 };
